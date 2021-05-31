141b1a6bcca05c9673732b5a7e8ae831d233fbf4
Victor <lausellv@yahoo.com>
2021-06-01 00:24:53 +0200

    formatted w/prettier (plyd arnd a bit)

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,15 +1,36 @@
-import React from 'react';
-import { MovieCard } from '../movie-card/movie-card';
-import {MovieView} from '../movie-view/movie-view';
+import React from "react";
+import { MovieCard } from "../movie-card/movie-card";
+import { MovieView } from "../movie-view/movie-view";
 
 export default class MainView extends React.Component {
   constructor() {
     super();
     this.state = {
       movies: [
-       { _id: 1, Title: 'Inception', Description: 'A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.', ImagePath: 'https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_UX67_CR0,0,67,98_AL_.jpg' },
-        { _id: 2, Title: 'The Shawshank Redemption', Description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.', ImagePath: 'https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX67_CR0,0,67,98_AL_.jpg' },
-        { _id: 3, Title: 'Gladiator', Description: 'A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.', ImagePath: 'https://m.media-amazon.com/images/M/MV5BMDliMmNhNDEtODUyOS00MjNlLTgxODEtN2U3NzIxMGVkZTA1L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX67_CR0,0,67,98_AL_.jpg'},
+        {
+          _id: 1,
+          Title: "Inception",
+          Description:
+            "A thief who steals corporate secrets through the use of dream-sharing technology is given the inverse task of planting an idea into the mind of a C.E.O.",
+          ImagePath:
+            "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_UX67_CR0,0,67,98_AL_.jpg"
+        },
+        {
+          _id: 2,
+          Title: "The Shawshank Redemption",
+          Description:
+            "Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.",
+          ImagePath:
+            "https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX67_CR0,0,67,98_AL_.jpg"
+        },
+        {
+          _id: 3,
+          Title: "Gladiator",
+          Description:
+            "A former Roman General sets out to exact vengeance against the corrupt emperor who murdered his family and sent him into slavery.",
+          ImagePath:
+            "https://m.media-amazon.com/images/M/MV5BMDliMmNhNDEtODUyOS00MjNlLTgxODEtN2U3NzIxMGVkZTA1L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_UX67_CR0,0,67,98_AL_.jpg"
+        }
       ],
       selectedMovie: null
     };
@@ -24,18 +45,31 @@ export default class MainView extends React.Component {
   render() {
     const { movies, selectedMovie } = this.state;
     // if (selectedMovie) return <MovieView movie={selectedMovie} />;
-    if (movies.length === 0) return <div className="main-view">This list is empty!</div>;
-
-    return (
-      <div className="main-view">
-        
-        {selectedMovie
-          ? <MovieView movie={selectedMovie} onBackClick={newSelectedMovie => { this.setSelectedMovie(newSelectedMovie) }}/>
-          : movies.map(movie => (
-            <MovieCard key={movie._id} movie={movie} onMovieClick={(movie) => { this.setSelectedMovie(movie) }}/>
-          ))
-        }
-      </div>
-    );
+    if (movies.length === 0) {
+      return <div className="main-view">This list is empty!</div>;
+    } else {
+      return (
+        <div className="main-view">
+          {selectedMovie ? (
+            <MovieView
+              movie={selectedMovie}
+              onBackClick={newSelectedMovie => {
+                this.setSelectedMovie(newSelectedMovie);
+              }}
+            />
+          ) : (
+            movies.map(movie => (
+              <MovieCard
+                key={movie._id}
+                movieData={movie}
+                onMovieClick={movie => {
+                  this.setSelectedMovie(movie);
+                }}
+              />
+            ))
+          )}
+        </div>
+      );
+    }
   }
 }
--- src/components/movie-card/movie-card.jsx
+++ src/components/movie-card/movie-card.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export class MovieCard extends React.Component {
   render(){
-    const {movie, onMovieClick} = this.props;
-    return (<div className="movie-card" onClick={()=>{onMovieClick(movie)}}>{movie.Title}</div>)
+    const {movieData, onMovieClick} = this.props;
+    return (<div className="movie-card" onClick={()=>{onMovieClick(movieData)}}>{movieData.Title}</div>);
   }
 }
