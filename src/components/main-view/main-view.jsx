ce4df679d2a65c315a8bf41e47bffb829781ce5a
Victor <lausellv@yahoo.com>
2021-06-10 21:17:48 +0200

    added onregister & login meth impt login & regist

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import axios from "axios";
+import { LoginView } from "../login-view/login-view";
+import { RegistrationView } from "../registration-view/registration-view";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 
@@ -8,25 +10,37 @@ export default class MainView extends React.Component {
     super();
     this.state = {
       movies: [],
-      selectedMovie: null
+      selectedMovie: null,
+      user: null
     };
-  
   }
   // j
-// simply to see how to add an eventlistener once the component is mounted
-//   keypressCallback(event){
-//     console.log(event.key);
-//   }
+  // simply to see how to add an eventlistener once the component is mounted
+  //   keypressCallback(event){
+  //     console.log(event.key);
+  //   }
 
+  /*Upon clicking on a movie, the state is updated to it */
   setSelectedMovie(newSelectedMovie) {
     this.setState({
       selectedMovie: newSelectedMovie
     });
   }
 
+  /*Upon login in, the state is updated to such user */
+  onLoggedIn(user) {
+    this.setState({ user: user });
+  }
+
+  onRegister(register) {
+    this.setState({ register: register });
+  }
+
   render() {
-    const { movies, selectedMovie } = this.state; // same as const movies = this.state.movies & const selecteMovie = this.state.selectedMovie (ES6 object destructuring)
-    // if (selectedMovie) return <MovieView movie={selectedMovie} />;
+    const { movies, selectedMovie, user } = this.state; // same as const movies = this.state.movies & const selecteMovie = this.state.selectedMovie (ES6 object destructuring)
+
+    if (!user) return <LoginView onLoggedIn={user => this.onLoggedIn(user)} />;
+
     if (movies.length === 0) {
       return <div className="main-view" />;
     } else {
@@ -64,15 +78,11 @@ export default class MainView extends React.Component {
       .catch(error => {
         console.log(error);
       });
-// an example of how to use evntlistener after the main-view component has been mounted
-     // document.addEventListener('keypress', this.keypressCallback)
-    }
-
-    // componentWillUnmount(){
-    //   document.removeEventListener('keypress', this.keypressCallback)
-    // }
-      
+    // an example of how to use evntlistener after the main-view component has been mounted
+    // document.addEventListener('keypress', this.keypressCallback)
   }
 
-
-
+  // componentWillUnmount(){
+  //   document.removeEventListener('keypress', this.keypressCallback)
+  // }
+}
