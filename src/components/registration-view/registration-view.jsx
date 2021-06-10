e65761a1da5c26579959f03fd52c79990f08384b
Victor <lausellv@yahoo.com>
2021-06-10 21:15:37 +0200

    created login-view & registration-view

---
+++ src/components/registration-view/registration-view.jsx
@@ -0,0 +1,54 @@
+import React, { useState } from "react";
+
+export function RegistrationView(props) {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [birthday, setBirthday] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(username, password, email, birthday);
+  };
+
+  return (
+    <form>
+      <label>
+        Username:
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </label>
+      <label>
+        Create Password:
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <label>
+          Email:
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </label>
+        <label>
+          Birthday:
+          <input
+            type="date"
+            value={birthday}
+            onChange={(e) => setBirthday(e.target.value)}
+          />
+        </label>
+      </label>
+      <button type="submit" onClick={handleSubmit}>
+        <button onClick={() => { onBackClick(null); }}>Back</button>
+        Submit
+      </button>
+    </form>
+  );
+}
