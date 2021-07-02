44fb82799d885e4432d3a2e37dc5dce49ab926c3
Victor <lausellv@yahoo.com>
2021-07-02 19:48:46 +0200

    created director view

--- src/components/director-view/director-view.jsx
+++ src/components/director-view/director-view.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
 import { Card, Button } from 'react-bootstrap';
-import { Link } from "react-router-dom";
-
+import PropTypes from 'prop-types';
+import './director-view.scss';
 
 export class DirectorView extends React.Component{
     render(){
-        const {directorData } = this.props;
+        const {directorData, onBackClick } = this.props;
 
         return(
             <div className="director-view">
                
             <Card border="dark" className="mb-3" className="mt-4">
              <Card.Body>
-             <Card.Title><span className='text-primary'></span> {directorData.Name}</Card.Title>
-            <Card.Text><span className='text-primary'></span>{directorData.Bio}</Card.Text>
-            <Card.Text><span className='text-primary'></span>{directorData.Birth}</Card.Text>
-            <Link to={`/`}>
-                   <Button variant="outline-info">Back</Button>
-               </Link>
+             <Card.Title><span className='text-primary'></span> {directorData.Director.Name}</Card.Title>
+            <Card.Text><span className='text-primary'></span>{directorData.Director.Bio}</Card.Text>
+            <Card.Text><span className='text-primary'></span>{directorData.Director.Birth}</Card.Text>
+            <Button variant="secondary" size="sm" onClick={() => { onBackClick(null); }}>Back</Button>
              </Card.Body>
              </Card>
     
@@ -26,3 +24,13 @@ export class DirectorView extends React.Component{
         )
     }
 }
+
+DirectorView.propTypes = {
+   
+        directorData: PropTypes.shape({
+            Name: PropTypes.string.isRequired,
+            Bio: PropTypes.string.isRequired,
+            Birth: PropTypes.string.isRequired
+          }).isRequired,
+    onBackClick: PropTypes.func.isRequired
+  };
