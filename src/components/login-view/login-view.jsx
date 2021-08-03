406380d34b3d791bc496d943e0247c4f62d0db00
Victor <lausellv@yahoo.com>
2021-08-03 19:02:40 +0200

    connect to redux

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -3,14 +3,15 @@ import { Form, Button } from "react-bootstrap";
 import PropTypes from "prop-types";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
 import { FormGroup, Label, Input } from "reactstrap";
 
 import "./login-view.scss";
 export function LoginView(props) {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
-  const [usernameError, setUsernameError] = useState({});
-  const [passwordError, setPasswordError] = useState({});
+  // const [usernameError, setUsernameError] = useState({});
+  // const [passwordError, setPasswordError] = useState({});
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -45,12 +46,14 @@ export function LoginView(props) {
     if (username.trim().length < 5) {
       usernameError.usernameShort = "Username must be at least 5 characters";
       alert(usernameError.usernameShort);
+      // setUsernameError(usernameError);
       isValid = false;
     }
 
     if (password.trim().length === 0) {
       passwordError.passwordRequired = "Password is required";
       alert(passwordError.passwordRequired);
+      // setPasswordError(passwordError);
       isValid = false;
     }
 
@@ -60,8 +63,8 @@ export function LoginView(props) {
       isValid = false;
     }
 
-    setUsernameError(usernameError);
-    setPasswordError(passwordError);
+    
+    
     return isValid;
   };
 
@@ -86,11 +89,7 @@ export function LoginView(props) {
         </Button>
       </span>
       <Link to="/register">
-        <Button
-          className="super-button"
-          variant="outline-info"
-          type="secondary"
-        >
+        <Button className="super-button" variant="outline-info" type="secondary">
           REGISTER
         </Button>
       </Link>
@@ -106,3 +105,9 @@ LoginView.propTypes = {
   onLoggedIn: PropTypes.func.isRequired,
   onRegister: PropTypes.func
 };
+
+const mapDispatchToProps = (dispatch) => ({
+  handleSubmit: (username, password) => dispatch(handleSubmit(username, password))
+});
+
+export default connect(null, mapDispatchToProps)(LoginView);
