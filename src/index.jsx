722662e9f4fd86f58a4d92644d3675e25aac042d
Victor <lausellv@yahoo.com>
2021-08-03 16:44:52 +0200

    createStore, Provider is now the new "container"

--- src/index.jsx
+++ src/index.jsx
@@ -2,16 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom";
 import MainView from "./components/main-view/main-view";
 import Container from "react-bootstrap/Container";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import moviesApp from "./reducers/reducers";
+import { devToolsEnhancer } from "redux-devtools-extension";
 
-// import statmement to indicate that you need to bundle './index.scss
+const store = createStore(moviesApp, devToolsEnhancer());
+
+
 import "./index.scss";
 // Main component ( will eventually use all the others)
 class MyFlixApplication extends React.Component {
   render() {
     return (
-      <Container>
-        <MainView />
-      </Container>
+      <Provider store={store}>
+        <Container className="Main-Content-Container mt-4b">
+          <MainView />
+        </Container>
+      </Provider>
     );
   }
 }
