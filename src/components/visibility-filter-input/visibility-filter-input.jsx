938792ba640e7ecd667acca106a105cb223f9221
Victor <lausellv@yahoo.com>
2021-08-03 19:02:18 +0200

    filter to filter movies

--- src/components/visibility-filter-input/visibility-filter-input.jsx
+++ src/components/visibility-filter-input/visibility-filter-input.jsx
@@ -13,7 +13,7 @@ function VisibilityFilterInput(props) {
     <Form.Control
       onChange={e => props.setFilter(e.target.value)}
       value={props.visibilityFilter}
-      placeholder="filter"
+      placeholder="filter movies"
     />
   );
 }
