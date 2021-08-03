93d2a097df60a93d3f0c643928bfe1355c06ec30
Victor <lausellv@yahoo.com>
2021-08-03 16:53:51 +0200

    visibility-filter-input

---
+++ src/components/visibility-filter-input/visibility-filter-input.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+
+import Form from "react-bootstrap/Form";
+
+import "./visibility-filter-input.scss";
+
+import { setFilter } from "../../actions/actions";
+
+function VisibilityFilterInput(props) {
+  return (
+    <Form.Control
+      onChange={e => props.setFilter(e.target.value)}
+      value={props.visibilityFilter}
+      placeholder="filter"
+    />
+  );
+}
+
+VisibilityFilterInput.propTypes = {
+  setFilter: PropTypes.func.isRequired,
+  visibilityFilter: PropTypes.string
+};
+
+export default connect(null, { setFilter })(VisibilityFilterInput);
