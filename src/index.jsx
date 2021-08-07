6cc0a248c40af72f8685cec03d26182dc4aadc55
Victor <lausellv@yahoo.com>
2021-08-07 20:21:40 +0200

    index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,16 +1,15 @@
-import React from "react";
+import React, { Component } from "react";
 import ReactDOM from "react-dom";
-import MainView from "./components/main-view/main-view";
-import Container from "react-bootstrap/Container";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
+import MainView from "./components/main-view/main-view";
+import Container from "react-bootstrap/Container";
 import moviesApp from "./reducers/reducers";
 import { devToolsEnhancer } from "redux-devtools-extension";
+import "./index.scss";
 
 const store = createStore(moviesApp, devToolsEnhancer());
 
-
-import "./index.scss";
 // Main component ( will eventually use all the others)
 class MyFlixApplication extends React.Component {
   render() {
